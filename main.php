64edcb0aeec14b17e43cab1ec326463525e60a91
Rian Fadli <rianfadli11@gmail.com>
2019-10-12 13:48:23 +0700

    oke

--- main.php
+++ main.php
@@ -1,22 +1,7 @@
 <div id="kiri">
-	<div id="menu-kategori">
-		<ul>
-			<?php
-			$query = mysqli_query($koneksi, "SELECT * FROM kategori WHERE status='on'");
-			while ($row = mysqli_fetch_array($query)) {
-
-				if($kategori_id == $row['kategori_id']){
-
-					echo "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]' class='active'>$row[kategori]</a></li>";
-					
-				}else{
-			 	
-			 		echo "<li><a href='".BASE_URL."index.php?kategori_id=$row[kategori_id]'>$row[kategori]</a></li>";
-				}
-			}
-			?>
-		</ul>
-	</div>
+	<?php
+		echo kategori($kategori_id);
+	?>
 </div>
 
 
