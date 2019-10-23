8a6da34e17b32727ecd9acec0e2ed0391ebd78c8
Rian Fadli <rianfadli11@gmail.com>
2019-10-23 14:05:42 +0700

    pagination

--- module/kategori/list.php
+++ module/kategori/list.php
@@ -3,7 +3,11 @@
 </div>
 
 <?php
-	$queryKategori = mysqli_query($koneksi, "SELECT * FROM kategori");
+	$pagination = isset($_GET["pagination"]) ? $_GET["pagination"] : 1;
+	$data_per_halaman = 3;
+	$mulai_dari = ($pagination-1) * $data_per_halaman;
+
+	$queryKategori = mysqli_query($koneksi, "SELECT * FROM kategori LIMIT $mulai_dari, $data_per_halaman");
 
 	if(mysqli_num_rows($queryKategori) == 0){
 		echo "<h3>Saat ini belum ada data kategori</h3>";
@@ -17,7 +21,7 @@
 				<th class='tengah'>Action</th>
 			  </tr>";
 
-		$no =1;
+		$no=1;
 		while($row = mysqli_fetch_assoc($queryKategori)){
 
 			echo "<tr>
@@ -32,5 +36,28 @@
 		}
 
 		echo "</table>";
+
+		$queryHitungKategori = mysqli_query($koneksi, "SELECT * FROM kategori");
+		$total_data = mysqli_num_rows($queryHitungKategori);
+		$total_halaman = ceil($total_data / $data_per_halaman);
+
+		echo "<ul class='pagination'>";
+		for($i=1; $i<=$total_halaman; $i++)
+		{
+
+			if($pagination == $i)
+			{
+				
+				echo "<li><a class='active' href='".BASE_URL."index.php?page=my_profile&module=kategori&action=list&pagination=$i'>".$i."</a></li>";
+
+			}
+			else
+			{
+
+				echo "<li><a href='".BASE_URL."index.php?page=my_profile&module=kategori&action=list&pagination=$i'>".$i."</a></li>";
+
+			}
+		}
+		echo "</ul>";
 	}
 ?>
